c45237aa210b97b7bc7035ba5e9f4cb3baf94024
designernagesh <designer.nagesh@gmail.com>
2023-12-05 12:28:41 +0530

    modified styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,48 @@
 import { useState } from "react";
 
 function App() {
-
   const [inputNumber, setInputNumber] = useState("");
-  const [randomNumber, setRandomNumber] = useState(0);
+  const [randomNumber, setRandomNumber] = useState("");
   const [count, setCount] = useState(0);
   const [message, setMessage] = useState("");
-  const [ guessedNumbers, setGuessedNumbers ] = useState([]);
+  const [guessedNumbers, setGuessedNumbers] = useState([]);
 
   let minNumber = 1;
   let maxNumber = 10;
 
   const inputHandler = (e) => {
     setInputNumber(e.target.value);
-  }
-  
-  const buttonHandler = () => {
-    if(inputNumber) {
-      let randomNumber = Math.floor(Math.random() * (maxNumber - minNumber) + 1) + minNumber;
-      setRandomNumber(randomNumber);
-      
-      setCount(prevCount => prevCount + 1);
-      
-      setGuessedNumbers( prevGuess => [
-        ...prevGuess,
-        inputNumber
-      ])
-      
-      setInputNumber("");
+  };
 
+  const buttonHandler = () => {
+    if (/^[0-9]+$/.test(inputNumber) && inputNumber) {
+      let randomNumber =
+        Math.floor(Math.random() * (maxNumber - minNumber) + 1) + minNumber;
+      setRandomNumber(randomNumber);
+
+      setCount((prevCount) => prevCount + 1);
+
+      setGuessedNumbers((prevGuess) => [...prevGuess, inputNumber]);
+
+      setInputNumber("");
+      
       if (inputNumber === randomNumber) {
-        setMessage('Yippie You WIN!');
-      }
-      else if (inputNumber < randomNumber) {
-        setMessage('Your Guess is LOW!');
-      }
-      else if (inputNumber > randomNumber) {
-        setMessage('Your Guess is HIGH!');
+        setMessage("Yippie You WIN!");
+      } else if (inputNumber < randomNumber) {
+        setMessage("Your Guess is LOW!");
+      } else if (inputNumber > randomNumber) {
+        setMessage("Your Guess is HIGH!");
       }
     } else {
-      alert("kk")
+      alert("Please enter a valid number");
+      setInputNumber("");
     }
-  }
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <div className="container">
@@ -49,14 +50,31 @@ function App() {
         <p className="sub-title">
           I am thinking of a Number between 1 - 10! 😊
         </p>
-        <p>Can you guess it?</p>
-
-        <input placeholder="Guess the No." type="text" value={inputNumber} onChange={inputHandler} />
-        {message}
-        The number was: {randomNumber}
-        No. of Guesses: {count}
-        Guessed numbers are: {guessedNumbers.join(", ")}
-        <button onClick={buttonHandler}>Guess the Number</button>
+        <div className="wrapper">
+          <form onSubmit={submitHandler}>
+            <input
+              placeholder="Guess the No."
+              type="text"
+              value={inputNumber}
+              onChange={inputHandler}
+            />
+            <button type="submit" onClick={buttonHandler}>
+              Guess the Number
+            </button>
+          </form>
+        </div>
+        <p className="message">{message}</p>
+        <div className="two-cols">
+          <p>
+            The number was: <strong>{randomNumber}</strong>
+          </p>
+          <p>
+            No. of Guesses: <strong>{count}</strong>
+          </p>
+        </div>
+        <p>
+          Guessed numbers are: <strong>{guessedNumbers.join(", ")}</strong>
+        </p>
       </div>
     </>
   );
